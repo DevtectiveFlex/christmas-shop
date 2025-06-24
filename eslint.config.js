d78222957195bd8f282f166123aa04889db9e64d
Ilya Alexeev <devtective.exe@gmail.com>
2025-06-24 15:13:31 +0300

    chore: initialize project with TypeScript, Vite, and ESLint configuration
    - Added package.json with development scripts and dependencies
    - Created tsconfig.json for TypeScript configuration
    - Added vite-env.d.ts for Vite client type definitions

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import eslintPlugin from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import globals from 'globals'
+import prettier from 'eslint-plugin-prettier';
+import tsParser from '@typescript-eslint/parser'
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+export default [
+  {
+    ignores: [
+      'dist/**',
+      '**/*.d.ts',
+    ]
+  },
+  
+  eslintPlugin.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginUnicorn.configs.recommended,
+ 
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: import.meta.dirname,
+        ecmaVersion: 2023,
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+     prettier,
+    },
+    rules: {
+      'unicorn/no-array-callback-reference': 'off',
+      'unicorn/no-array-for-each': 'error',
+      'unicorn/no-array-reduce': 'off',
+      'unicorn/no-null': 'off',
+      'unicorn/number-literal-case': 'off',
+      'unicorn/numeric-separators-style': 'off',
+      'unicorn/prefer-global-this': 'off',
+    },
+  },
+]
